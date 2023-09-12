8d0da7e5bf23435ca11c1e9c6bbe56e1e124c40b
Angelica <alossada@gmail.com>
2023-09-12 16:47:58 -0500

    Exercise week 1

---
+++ exercice_01.js
@@ -0,0 +1,130 @@
+let numberOne= 1;
+let numbertwo= 2;
+
+// Ejercicio 1
+function add(numberOne, numbertwo){
+    let addResult;
+    addResult= numberOne + numbertwo;
+    console.log(addResult);
+    return addResult;
+}
+
+console.log(add(numberOne, numbertwo));
+console.log(add(7, 8));
+console.log(add("2", 2));
+
+// Ejercicio 2
+function multiply(numberOne, numbertwo){
+    if(Number.isInteger(numberOne) && Number.isInteger(numbertwo)){
+        multiplyResult= numberOne * numbertwo;
+        console.log(multiplyResult);
+        return multiplyResult;
+    } else {
+        console.log("Ambos parámetros deben ser números.");
+        return "Ambos parámetros deben ser números.";
+    }
+}
+
+console.log(multiply(numberOne, numbertwo));
+console.log(multiply(7, 8));
+console.log(multiply("2", 2));
+
+// Ejercicio 3
+let month = "mayo";
+
+function isWinter (month){
+    if(month == "junio" || month == "julio" || month == "agosto"){
+        console.log(true);
+        return true;
+    } else{
+        console.log(false);
+        return false;
+    }
+}
+
+console.log(isWinter(month));
+console.log(isWinter("junio"));
+console.log(isWinter("enero"));
+
+// Ejercicio 4
+let covidSymptoms = ["fiebre", "dolor muscular", "pérdida de gusto"];
+
+let sickSymptoms = ["fiebre", "dolor muscular", "vómitos"];
+let noSickSymptoms = ["fiebre", "titila ojo", "vómitos"];
+
+
+function isCovid(patientSymptoms, covidSymptoms){
+    let matchSymptoms = 0;
+
+    for(i = 0; i < patientSymptoms.length; i++){     
+        for(j = 0; j < covidSymptoms.length; j++){
+            if(patientSymptoms[i] == covidSymptoms[j]){
+                matchSymptoms++;
+            }              
+        }
+    }
+
+    if(matchSymptoms >= 2){
+        console.log(true);
+        console.log(matchSymptoms);
+        return true;
+    }else{
+        console.log(false);
+        console.log(matchSymptoms);
+        return false;
+    } 
+}
+
+function isCovidTwo(patientSymptoms, covidSymptoms){
+    let matchSymptoms = 0;
+
+    for(i = 0; i < patientSymptoms.length; i++){
+        if(covidSymptoms.includes(patientSymptoms[i])){
+            matchSymptoms++;
+        }
+    }
+
+    if(matchSymptoms >= 2){
+        console.log(true);
+        console.log(matchSymptoms);
+        return true;
+    }else{
+        console.log(false);
+        console.log(matchSymptoms);
+        return false;
+    } 
+}
+
+console.log(isCovid(["fiebre", "diarrea", "dolor muscular", "pérdida de gusto"], covidSymptoms));
+console.log(isCovid(noSickSymptoms, covidSymptoms));
+console.log(isCovid(sickSymptoms, covidSymptoms));
+
+console.log(isCovidTwo(["fiebre", "diarrea", "dolor muscular", "pérdida de gusto"], covidSymptoms));
+console.log(isCovidTwo(noSickSymptoms, covidSymptoms));
+console.log(isCovidTwo(sickSymptoms, covidSymptoms));
+
+// Ejercicio 5
+function showSchedule(subject){
+    switch(subject){
+        case "matemática":
+            return "jueves";
+            break;
+
+        case "lengua":
+            return "lunes";
+            break;
+
+        case "geografia":
+            return "viernes";
+            break;
+        
+        default:
+            return "MATERIA NO ENCONTRADA."
+    }
+}
+
+console.log(showSchedule("matemática"));
+console.log(showSchedule("lengua"));
+console.log(showSchedule("geografia"));
+console.log(showSchedule("programación"));
+
